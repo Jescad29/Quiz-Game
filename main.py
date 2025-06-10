df6491d7aa9de6bbe9da1075d1aebb119673f6be
Chuy <jescad27@gmail.com>
2025-06-10 17:14:10 -0600

    creacion de mas data

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from quiz_brain import QuizBrain
 question_bank = []
 
 for question in question_data:
-    question_text = question["text"]
-    question_answer = question["answer"]
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
     new_question = Question(q_text=question_text, q_answer=question_answer)
     question_bank.append(new_question)
 
@@ -14,3 +14,8 @@ quiz = QuizBrain(question_bank)
 
 while quiz.still_has_question():
     quiz.next_question()
+
+print("You've completed the quiz")
+num_question = len(question_bank)
+
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
